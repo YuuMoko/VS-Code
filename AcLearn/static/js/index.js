9480d1b33b4c46834bd39fce2814bfec793b34ae
YuuMoko <2772337221@qq.com>
2023-11-23 22:30:21 +0800

    TodoList work

--- AcLearn/static/js/index.js
+++ AcLearn/static/js/index.js
@@ -1,12 +1,8 @@
-let input = document.querySelector(".input");
-let run = document.querySelector("button");
-let output = document.querySelector("pre");
-
-function main() {
-    run.addEventListener("click", function () {
-        let s = input.value;
-        output.innerHTML = s;
-    });
+let main = function () {
+    let starttime = new Date();
+    console.log(starttime.getSeconds());
+    let $div = $('div');
+    $div.
 }
 
 
