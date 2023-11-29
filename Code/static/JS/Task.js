113bae033d2b6c2ef6c558cd3cbbcf558e5d66b9
YuuMoko <2772337221@qq.com>
2023-11-29 22:25:46 +0800

    TodoList 11/29

--- Code/static/JS/Task.js
+++ Code/static/JS/Task.js
@@ -3,15 +3,17 @@ let $bt = $('#create');
 let $input = $('.nav input');
 let $left = $('.left');
 let $right = $('.right');
+
+// This is a function used to create a Task and add functionality to the components of each Task
 let NewTask = function () {
 
     let text = $input.val();
-
+    // If the input is empty, an error will be reported
     if (text == "") {
         alert("You must write something!");
         return 0;
     }
-
+    // Set sub elements for Task
     let $task = $("<div class='task'></div>");
     let $p = $('<p>' + text + '</p>');
     $task.append($p);
@@ -21,13 +23,18 @@ let NewTask = function () {
     $task.append('<button class="edit">Edit</button>');
     $left.append($task);
 
+    // Set the deletion function. If you click this button, the corresponding task will be deleted
     let $del = $task.find('.del');
     $del.on('click', function () {
-        $task.remove();
+        let p = confirm("Do you really want to delete it?");
+        if (!p) return;
+        $task.fadeOut(200, function () {
+            $task.remove();
+        });
     });
 
     let $check = $task.find('input');
-
+    // Determine the location of the task based on the checkbox, move it to another location, and add a strikethrough to it
     $check.on('click', function () {
         if (!$check.is(':checked')) {
             $left.append($task);
@@ -39,8 +46,37 @@ let NewTask = function () {
         }
     });
 
+    // Editing function
+
     let $edit = $task.find('.edit');
+
+    // add a change function for $edit
+
+    let flag = true;
+
+    $edit.on('click', function (e) {
+        if (flag) flag = false;
+        else flag = true;
+    });
+
+    $edit.on('click', function (e) {
+        if (!flag) return;
+        let $change = $task.find('.change');
+        if ($task.children('.change').length == 0) return;
+        if ($change.val() == "") {
+            if (flag) flag = false;
+            else flag = true;
+            alert("You must write something!");
+            return;
+        }
+        $p.text($change.val());
+        $change.remove();
+
+    });
+
     $edit.on('click', function () {
+        if (flag) return;
+        // Similarly, if the input is empty, modification is not allowed
         if (($task.children('.change').length > 0)) return;
         $task.append("<input type='text' class='change' placeholder='Enter change'/>");
         let $change = $task.find('.change');
@@ -50,6 +86,8 @@ let NewTask = function () {
                     alert("You must write something!");
                     return 0;
                 }
+                if (flag) flag = false;
+                else flag = true;
                 $p.text($change.val());
                 $change.remove();
             }
@@ -58,7 +96,7 @@ let NewTask = function () {
 
 
 }
-
+// Main function, mainly set to enter and click the Create key to trigger the New function
 let main = function () {
 
     $bt.on('click', function () {
--- Code/static/JS/Time.js
+++ Code/static/JS/Time.js
@@ -1,4 +1,4 @@
-
+// Retrieve the current time and print it under Create Input
 let $time = $('.Time');
 let Time = function () {
     let date = new Date();
@@ -8,9 +8,9 @@ let Time = function () {
     let day = date.getDate();
     let hour = date.getHours();
     let minutes = date.getMinutes();
-    $time.text(year + "/" + month + "/" + day + ":" + hour + ":" + minutes);
+    $time.text(year + "/" + month + "/" + day + " " + hour + ":" + minutes);
 }
-
+// Execute time function every 300 milliseconds
 setInterval(() => {
     Time();
 }, 300);
